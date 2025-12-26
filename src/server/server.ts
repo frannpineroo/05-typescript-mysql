185136305c5e8d6b3f233192df34a6ea030eccf4
franpi <frannpinero@hotmail.com>
2025-12-26 19:23:13 -0300

    Desplegar el public folder

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,11 @@
 import type { Application } from 'express';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export default class Server {
     public app: express.Application;
@@ -14,7 +20,13 @@ export default class Server {
         return new Server( port );
     }
 
+    private publicFolder() {
+        const publicPath = path.resolve( __dirname, '../public' );
+        this.app.use( express.static( publicPath ) );
+    }
+
     start ( callback?: (error?: Error) => void ) {
         this.app.listen( this.port, callback );
+        this.publicFolder();
     }
 }
